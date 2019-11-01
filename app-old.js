6dcec5f1e9addea04716cd07d173eaf1f59a0d26
Roman <ro.28745.maydana@gmail.com>
2019-11-01 00:10:33 -0400

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+
+        'Content-Type': 'aplication/json'
+    });
+
+    let salida = {
+        nombre: 'Roman',
+        edad: 24,
+        url: req.url
+    }
+    res.write(JSON.stringify(salida));
+    res.end();
+})
+.listen(3000);
+
+console.log('Escuchando el puerto 8080');
